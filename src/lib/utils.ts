c2b07bc63716d89601a50ae5d649bd7c0bee5352
jinnai <jinnai.shirakashi@logical-fabrics.com>
2021-08-04 18:10:56 +0900

    非同期関数　修正

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,5 @@
+import { user } from 'firebase-functions/lib/providers/auth'
+import { resolve } from 'path/posix'
 import { db, auth } from './firebase'
 
 export const sendEmail = async (email: string) => {
@@ -5,6 +7,7 @@ export const sendEmail = async (email: string) => {
     alert('メールアドレスを入力してください')
     return
   }
+  // const callback = 'http://localhost:3000'
   const callback = 'https://pineapple-8d06c.web.app/'
 
   const actionCodeSettings = {
@@ -13,19 +16,14 @@ export const sendEmail = async (email: string) => {
   }
 
   window.localStorage.setItem('emailForSignIn', email), 'setitem'
-
-  return new Promise(() => {
-    auth
-      .sendSignInLinkToEmail(email, actionCodeSettings)
-      .then(() => {
-        alert('メールを送信しました')
-      })
-      .catch((error) => {
-        console.log(error.code)
-        console.log(error.message)
-        alert(error.message)
-      })
-  })
+  try {
+    await auth.sendSignInLinkToEmail(email, actionCodeSettings)
+    alert('メールを送信しました')
+  } catch (error) {
+    console.log(error.code)
+    console.log(error.message)
+    alert(error.message)
+  }
 }
 
 export const emailSignin = async () => {
@@ -33,46 +31,39 @@ export const emailSignin = async () => {
   console.log(window.localStorage.getItem('emailForSignIn'), 'getitem')
   let email = localStorage.getItem('emailForSignIn')
 
-  return new Promise(() => {
-    if (auth.isSignInWithEmailLink(window.location.href)) {
-      if (!email) {
-        email = window.prompt('確認のためにメールアドレスを入力してください')
-      }
-      auth
-        .signInWithEmailLink(email, window.location.href)
-        .then(() => {
-          window.localStorage.removeItem('emailForSignIn')
-          auth.onAuthStateChanged((user) => {
-            const uid = user.uid
-            console.log('login success', uid)
-            db.collection('users').doc(uid).set({
-              uid: uid,
-              contactlist: [],
-            })
-          })
+  if (auth.isSignInWithEmailLink(window.location.href)) {
+    if (!email) {
+      email = window.prompt('確認のためにメールアドレスを入力してください')
+    }
+
+    try {
+      await auth.signInWithEmailLink(email, window.location.href)
+      window.localStorage.removeItem('emailForSignIn')
+      auth.onAuthStateChanged((user) => {
+        const uid = user.uid
+        console.log('signup success', uid)
+        db.collection('users').doc(uid).set({
+          uid: uid,
+          contactlist: [],
         })
-        .catch((error) => {
-          console.log('エラー', error)
-          window.localStorage.removeItem('emailForSignIn')
-        })
-    } else {
-      console.log('not emailLink')
+      })
+    } catch (error) {
+      console.log('エラー', error)
       window.localStorage.removeItem('emailForSignIn')
     }
-  })
+  } else {
+    console.log('not emailLink')
+    window.localStorage.removeItem('emailForSignIn')
+  }
 }
 
-export const listenAuthstate = async () => {
-  return new Promise((resolve) => {
-    auth.onAuthStateChanged((user: any) => {
-      if (user != null) {
-        // ユーザー情報取得成功
-        console.log(user, 'signin')
-        resolve(user)
-      } else {
-        console.log('null')
-        resolve(null)
-      }
-    })
+export const listenAuthstate = () => {
+  auth.onAuthStateChanged((user: any) => {
+    if (user != null) {
+      // ユーザー情報取得成功
+      console.log(user, 'signin')
+    } else {
+      console.log('null')
+    }
   })
 }
