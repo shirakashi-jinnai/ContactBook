02e6935e43b22b656703457bbb7b267e1041c44e
jinnai <jinnai.shirakashi@logical-fabrics.com>
2021-08-14 18:11:39 +0900

    hooksにfirestore呼び出し処理を記述

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,20 +11,6 @@ const Home = () => {
   const { user, setUser } = useContext(UserContext)
   console.log(user)
 
-  // useEffect(() => {
-  //   const unsub = db
-  //     .doc(`users/${user.uid}`)
-  //     .collection('contacts')
-  //     .onSnapshot((s) => {
-  //       const items = _.map(s.docs, (doc) => {
-  //         return { id: doc.id, ...doc.data() }
-  //       })
-  //       const data = { ...user, contacts: items }
-  //       setUser(data)
-  //     })
-  //   return () => unsub()
-  // }, [])
-
   return (
     <Layout title={'連絡帳'}>
       <h1>hello nextjs</h1>
