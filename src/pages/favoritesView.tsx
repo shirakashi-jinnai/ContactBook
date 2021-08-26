4969d43072efb7eb9a6e254f132a68d56fdde8f2
jinnai <jinnai.shirakashi@logical-fabrics.com>
2021-08-26 17:17:09 +0900

    お気に入りリストで検索結果を表示する機能の作成

--- src/pages/favoritesView.tsx
+++ src/pages/favoritesView.tsx
@@ -4,6 +4,7 @@ import Layout from '../components/Layout'
 import { UserContext } from '../lib/context'
 import { makeStyles } from '@material-ui/core'
 import EntryView from '../components/EntryView'
+import { searchItems } from '../lib/utils'
 
 const useStyles = makeStyles({
   viewArea: {
@@ -15,16 +16,37 @@ const useStyles = makeStyles({
 const FavoriteView = () => {
   const classes = useStyles()
   const { user } = useContext(UserContext)
-  const favorites = _.filter(user.contacts, 'liked')
+  const { contacts, isSearchKeywords, isSearchAgeRange, keywords, ageRange } =
+    user
+
+  const isSearching = isSearchKeywords || isSearchAgeRange
+  const favorites = _.filter(contacts, 'liked')
+
+  const items = searchItems(
+    favorites,
+    keywords,
+    ageRange.ranges,
+    ageRange.isLessThan
+  )
+
   return (
     <Layout title='お気に入りリスト'>
       <div className={classes.viewArea}>
+        {isSearching && (
+          <p>
+            {items.length}件/
+            {favorites.length}件のヒット
+          </p>
+        )}
+
         {!_.isEmpty(favorites) ? (
-          favorites.map((favorite: Entry, i: number) => (
-            <EntryView key={i} entry={favorite} />
-          ))
+          (isSearching ? items : favorites).map(
+            (favorite: Entry, i: number) => (
+              <EntryView key={i} entry={favorite} />
+            )
+          )
         ) : (
-          <p>お気に入りが登録されていません。</p>
+          <p>連絡先が登録されていません。</p>
         )}
       </div>
     </Layout>
