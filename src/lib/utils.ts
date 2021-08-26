78a77ad92e6d99a688b76e8aed41fdb891042500
jinnai <jinnai.shirakashi@logical-fabrics.com>
2021-08-26 17:04:35 +0900

    検索機能の作成

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,4 @@
+import _ from 'lodash'
 import { db, auth } from './firebase'
 
 export const sendEmail = async (email: string) => {
@@ -24,3 +25,51 @@ export const sendEmail = async (email: string) => {
   }
 }
 
+//birthday から年齢を求める
+const ageCounter = (birthday: string): number => {
+  const now = new Date()
+  const year = now.getFullYear()
+  const month = now.getMonth() + 1
+  const date = now.getDate()
+  const splitBirthday = birthday.split('-')
+  const birthdayYear = Number(splitBirthday[0])
+  const birthdayMonth = Number(splitBirthday[1])
+  const birthdayDate = Number(splitBirthday[2])
+
+  let age = year - birthdayYear
+  if (
+    birthdayMonth > month ||
+    (birthdayMonth == month && birthdayDate > date)
+  ) {
+    age -= 1
+  }
+  return age
+}
+
+export const searchItems = (
+  items,
+  keywords: string[],
+  ranges: string[],
+  isLessThan: boolean
+) => {
+  const searchKeywords = items.filter(
+    ({ firstName, lastName }) =>
+      keywords &&
+      keywords.every(
+        (kw: string) => (firstName + lastName).toLowerCase().indexOf(kw) !== -1
+      )
+  )
+  if (!ranges.length) {
+    return searchKeywords
+  }
+
+  const searchAgeRange = searchKeywords.filter(({ birthday }) => {
+    const age = ageCounter(birthday)
+    if (ranges.length !== 1) {
+      return age >= Number(ranges[0]) && age <= Number(ranges[1])
+    } else {
+      return isLessThan ? age < 10 : age >= 80
+    }
+  })
+  return searchAgeRange
+}
