e8b0018159b6d4c1d3aabfcf5f5c5388e5e99d4a
jinnai <jinnai.shirakashi@logical-fabrics.com>
2021-08-26 16:19:44 +0900

    initialStateの変更

--- src/lib/initialstate.ts
+++ src/lib/initialstate.ts
@@ -2,5 +2,9 @@ export const initialState = {
   user: {
     uid: '',
     contacts: [],
+    keywords: [],
+    ageRange: { ranges: [], isLessThan: false },
+    isSearchAgeRange: false,
+    isSearchKeywords: false,
   },
 }
