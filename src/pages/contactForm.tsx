69b4bb5d04acd1b5cc79fd4c9dbc53c1bd958854
shirakashi-jinnai <j.shirakashi.jb@gmail.com>
2021-09-26 19:16:31 +0900

    画像のアップロード、削除をmenuで行う処理の作成

--- src/pages/contactForm.tsx
+++ src/pages/contactForm.tsx
@@ -1,12 +1,13 @@
 import _ from 'lodash'
-import { useEffect, useReducer } from 'react'
+import { useEffect, useReducer, useState } from 'react'
 import { useRouter } from 'next/router'
 import Image from 'next/image'
 import { DateTime } from 'luxon'
 import shortid from 'shortid'
 import { auth, db, storage } from '../lib/firebase'
 import { makeStyles } from '@material-ui/styles'
-import { TextField } from '@material-ui/core'
+import { IconButton, Menu, MenuItem, TextField } from '@material-ui/core'
+import EditIcon from '@material-ui/icons/Edit'
 import Layout from '../components/Layout'
 import PrimaryButton from '../components/UIkit/PrimaryButton'
 import { TimestampConverter } from '../lib/TimestampConverter'
@@ -22,14 +23,13 @@ const useStyles = makeStyles({
     display: 'flex',
     justifyContent: 'center',
   },
-  label: {
+  imageArea: {
     margin: '0 auto',
   },
   image: {
     objectFit: 'cover',
     borderRadius: '50%',
     objectPosition: '50% 50%',
-    cursor: 'pointer',
   },
 })
 
@@ -39,6 +39,16 @@ const ContactForm = ({ id, title = '連絡先の登録' }) => {
   const { uid } = auth.currentUser
   const storageRef = storage.ref().child(uid)
   const fileName = shortid.generate()
+  const temporaryImg = sessionStorage.getItem(uid)
+
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+  const open = Boolean(anchorEl)
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
 
   const [contact, setContact] = useReducer(
     (state: Contact, data: Partial<Contact>) => _.merge({}, state, data),
@@ -101,14 +111,29 @@ const ContactForm = ({ id, title = '連絡先の登録' }) => {
     router.push('/')
   }
 
+  const deleteImage = () => {
+    sessionStorage.removeItem(uid)
+    setContact({ avatarImg: { path: '', id: '' } })
+  }
+
+  const deleteTemporaryImage = async () => {
+    sessionStorage.removeItem(uid)
+    await storageRef.child(contact.avatarImg.id).delete()
+    if (!id) return setContact({ avatarImg: { path: '', id: '' } })
+    db.doc(`users/${uid}/contacts/${id}`)
+      .get()
+      .then((s) => {
+        setContact({ avatarImg: s.data().avatarImg })
+      })
+  }
+
   const getTemporaryImage = () => {
-    const storageItem = sessionStorage.getItem(uid)
-    if (!storageItem) return
+    if (!temporaryImg) return
     storageRef
-      .child(storageItem)
+      .child(temporaryImg)
       .getDownloadURL()
       .then((url) => {
-        setContact({ avatarImg: { path: url, id: storageItem } })
+        setContact({ avatarImg: { path: url, id: temporaryImg } })
       })
   }
 
@@ -136,7 +161,7 @@ const ContactForm = ({ id, title = '連絡先の登録' }) => {
     <Layout title={title}>
       <div className={classes.contactArea}>
         <h1>{title}</h1>
-        <label className={classes.label}>
+        <div className={classes.imageArea}>
           <Image
             className={classes.image}
             alt='avatar'
@@ -144,13 +169,46 @@ const ContactForm = ({ id, title = '連絡先の登録' }) => {
             width={200}
             height={200}
           />
-          <input
-            accept='image/jpeg'
-            type='file'
-            style={{ display: 'none' }}
-            onChange={onImageChange}
-          />
-        </label>
+          <IconButton onClick={handleClick}>
+            <EditIcon />
+          </IconButton>
+
+          <Menu anchorEl={anchorEl} open={open} onClose={handleClose}>
+            <label>
+              <MenuItem>
+                <input
+                  accept='image/jpeg'
+                  type='file'
+                  style={{ display: 'none' }}
+                  onChange={(e) => {
+                    onImageChange(e)
+                    handleClose()
+                  }}
+                />
+                アップロード
+              </MenuItem>
+            </label>
+            {temporaryImg && (
+              <MenuItem
+                onClick={() => {
+                  handleClose()
+                  deleteTemporaryImage()
+                }}>
+                下書き画像を削除
+              </MenuItem>
+            )}
+            {contact.avatarImg.id && id && !temporaryImg && (
+              <MenuItem
+                onClick={() => {
+                  handleClose()
+                  deleteImage()
+                }}>
+                画像を削除
+              </MenuItem>
+            )}
+          </Menu>
+        </div>
+
         <TextField
           label='姓(必須)'
           required
