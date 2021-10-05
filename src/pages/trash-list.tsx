7d29ed4efbe16bfb83fb5b4e7d800fb3886fe424
shirakashi-jinnai <j.shirakashi.jb@gmail.com>
2021-10-05 17:36:45 +0900

    trash-listを仮作成

---
+++ src/pages/trash-list.tsx
@@ -0,0 +1,11 @@
+import Layout from '../components/Layout'
+
+const TrashList = () => {
+  return (
+    <Layout title='ゴミ箱リスト'>
+      <h1>garbage</h1>
+    </Layout>
+  )
+}
+
+export default TrashList
