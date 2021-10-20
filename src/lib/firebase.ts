cb43571bf080d1066c438eba98ca2e714b25888c
shirakashi-jinnai <j.shirakashi.jb@gmail.com>
2021-10-20 12:23:25 +0900

    storageをv9互換へ書き換え

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,11 +1,11 @@
 //utilsの中にあるfileは共通関数を記述
-import 'firebase/storage'
+import firebase from 'firebase/compat/app'
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
-import { getStorage } from 'firebase/storage'
+import 'firebase/compat/storage'
 
-export const firebaseConfig = initializeApp({
+export const firebaseConfig = {
   apiKey: process.env.FIREBASE_KEY,
   authDomain: process.env.FIREBASE_DOMAIN,
   projectId: process.env.FIREBASE_DATABASE,
@@ -13,8 +13,13 @@ export const firebaseConfig = initializeApp({
   messagingSenderId: process.env.FIREBASE_STORAGE_BUCKET,
   appId: process.env.FIREBASE_SENDER_ID,
   measurementId: process.env.FIREBASE_APPID,
-})
+}
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig)
+}
+initializeApp(firebaseConfig)
 
 export const auth = getAuth()
 export const db = getFirestore()
-export const storage = getStorage()
+export const storage = firebase.storage()
