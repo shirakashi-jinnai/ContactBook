4b23a07dd6415d368f01ebaced47d24db68c2547
shirakashi-jinnai <j.shirakashi.jb@gmail.com>
2021-11-10 01:04:30 +0900

    crontabへの書き換え、変数名、コードの修正

--- functions/index.js
+++ functions/index.js
@@ -5,7 +5,7 @@ const { DateTime } = require('luxon')
 admin.initializeApp()
 const db = admin.firestore()
 
-const getContactsId = async (user) => {
+const getExpiredContactIds = async (user) => {
   return await db
     .collection(`users/${user.id}/contacts`)
     .where('removeTime', '<=', DateTime.now().minus({ days: 30 }).toJSDate())
@@ -14,22 +14,24 @@ const getContactsId = async (user) => {
     .then((s) => _.map(s.docs, ({ id }) => ({ id })))
 }
 
-const check24HoursDeletedElement = async () => {
+const delete24HoursExpiredElement = async () => {
   const users = await db
     .collection('users')
     .get()
     .then((s) => _.map(s.docs, ({ id }) => ({ id })))
 
-  _.forEach(users, async (user) => {
-    _.forEach(await getContactsId(user), async (c) => {
-      await db.doc(`users/${user.id}/contacts/${c.id}`).delete()
-    })
+  _.map(users, async (user) => {
+    await Promise.all(
+      _.map(await getExpiredContactIds(user), (c) =>
+        db.doc(`users/${user.id}/contacts/${c.id}`).delete()
+      )
+    )
   })
 }
 
 exports.scheduledFunction = functions.pubsub
-  .schedule('every 24 hours')
+  .schedule('0 0 * * *')
   .timeZone('Asia/Tokyo')
   .onRun(async () => {
-    await check24HoursDeletedElement()
+    await delete24HoursExpiredElement()
   })
